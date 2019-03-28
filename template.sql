b0de1fbaa59ec0fd706d6140e98bcdb37f14c3e5
Guilherme Diaz- Berrio <guilherme.diazberrio@kindredgroup.com>
2019-03-28 16:17:10 +0000

    added params

--- template.sql
+++ template.sql
@@ -1,20 +1,28 @@
 with cleaned_input as (
-    select customer_id{% for p in predictors %}
+    select customer_id
+        {% for p in predictors %}
         , case when {{ p['name'] }} is null
             then {{ p['fill_na'] }}
             else {{ p['name'] }} end
-            as {{ p['name'] }} {% endfor %}{% if train %}
-        , income{% endif %}
-    from customer_attr{% if train %}
+            as {{ p['name'] }} 
+        {% endfor %}
+        {% if train %}
+        , income
+        {% endif %}
+    from customer_attr
+    {% if train %}
     where customer_id % 10 = 0 --10% sample
-        and income is not null{% endif %}
+        and income is not null
+    {% endif %}
 )
 {%if train %}
 select * from cleaned_input
 {% else %}
 select customer_id
-    , {{ coefs['intercept'] }}{% for p in predictors %}
-        + {{ coefs[p['name']] }}*{{ p['name'] }}{% endfor %}
+    , {{ coefs['intercept'] }}
+        {% for p in predictors %}
+        + {{ coefs[p['name']] }}*{{ p['name'] }}
+        {% endfor %}
         as predicted_income
 from cleaned_input
 {% endif %}
